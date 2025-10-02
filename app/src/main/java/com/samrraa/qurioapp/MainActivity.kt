e607d2e34d798663a1a0c38539594a54826dcbe2
Mohannad Ahmed <mohannad199344@gmail.com>
2025-10-02 10:22:55 +0300

    Refactor: Migrate to Android 12 splash screen API

--- app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
+++ app/src/main/java/com/samrraa/qurioapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.samrraa.qurioapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.samrraa.qurioapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
