303c73b0aa2f5dc4436321b3dc3c9374a1853f39
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-10-03 15:53:10 +0200

    feat: add games screen UI and logic

---
+++ app/src/main/java/com/samrraa/qurioapp/view/games/GamesAdapter.kt
@@ -0,0 +1,39 @@
+package com.samrraa.qurioapp.view.games
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.samrraa.qurioapp.R
+
+class GameCardAdapter(
+    private val items: List<Game>,
+    private val onButtonClick: (Game) -> Unit
+) : RecyclerView.Adapter<GameCardAdapter.GameCardViewHolder>() {
+
+    inner class GameCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.textTitle)
+        val button: Button = itemView.findViewById(R.id.startGameButton)
+        val image: ImageView = itemView.findViewById(R.id.cardImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameCardViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.small_game_card, parent, false)
+        return GameCardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GameCardViewHolder, position: Int) {
+        val game = items[position]
+        holder.title.text = game.title
+        holder.image.setImageResource(game.imageRes)
+        holder.button.setOnClickListener {
+            onButtonClick(game)
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+}
--- app/src/main/java/com/samrraa/qurioapp/view/games/GamesFragment.kt
+++ app/src/main/java/com/samrraa/qurioapp/view/games/GamesFragment.kt
@@ -1,13 +1,46 @@
 package com.samrraa.qurioapp.view.games
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import com.samrraa.qurioapp.R
 import com.samrraa.qurioapp.base.BaseFragment
 import com.samrraa.qurioapp.databinding.FragmentGamesBinding
 
 class GamesFragment : BaseFragment<FragmentGamesBinding>() {
+
     override fun initViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ) = FragmentGamesBinding.inflate(inflater, container, false)
+
+    private fun setupRecycler() {
+        val games = listOf(
+            Game("Music", R.drawable.music),
+            Game("Food & Drink", R.drawable.ic_food_and_coffe),
+            Game("Geography", R.drawable.green_image),
+            Game("General knowledge", R.drawable.knowladge),
+            Game("Television", R.drawable.tv),
+            Game("Family", R.drawable.family)
+        )
+
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerView.adapter = GameCardAdapter(games) { game ->
+            Toast.makeText(requireContext(), "Clicked on ${game.title}", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecycler()
+    }
 }
+
+data class Game(
+    val title: String,
+    val imageRes: Int
+)
